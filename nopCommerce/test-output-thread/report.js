1b2184a02f5a080d22ba65608e5c4b5cb98161d1
nanda <baskara.sadananda@gmail.com>
2023-07-20 15:53:39 +0530

    adding all the files

---
+++ nopCommerce/test-output-thread/report.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([{"id":"f959f2a7-f4fa-42a6-b7ef-108f5fe359dd","feature":"Login Feature","scenario":"Login with valid credentials","start":1689845251389,"group":1,"content":"","tags":"","end":1689845261101,"className":"passed"}]);
+CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[main,5,main]"}]);
+});
